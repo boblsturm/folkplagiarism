c2aaebadf602ec2c47691b8af74bdc79c913a770
Nestor Napoles Lopez <napulen@gmail.com>
2019-11-03 15:26:56 +0100

    Updating the interval_parser script

--- interval_parser.py
+++ interval_parser.py
@@ -1,24 +1,32 @@
 import music21
 import pprint
 
-if __name__ == '__main__':
-    with open('output_folkrnnv2.txt') as f:
+def compute_intervals(filename, file_limit):
+    with open(filename, encoding='utf-8') as f:
         data = f.read()
     # Files are delimited by a blank line (2 '\n's in a row )
     files = data.split('\n\n')
     # Files after being parsed by music21
-    m21files = []
-    for f in files[:100]:
-        # print(f)
+    all_intervals = []
+    for idx, f in enumerate(files[:file_limit]):
+        f = 'M:{}'.format(f.split('M:')[1])
         try:
             s = music21.converter.parseData(f)
             notes = s.flat.notes
-            # print(list(notes))
             intervals = []
             for n1, n2 in zip(notes[:-1], notes[1:]):
                 i = music21.interval.Interval(n1, n2)
-                intervals.append(i.semitones)
-            m21files.append(intervals)
+                # As long as one of the notes is not a rest
+                if hasattr(i, 'semitones'):
+                    intervals.append(i.semitones)
+            all_intervals.append(intervals)
         except:
-            print('crashed music21 -- {}'.format(f))
-    print(m21files)
+            """print('crashed music21 -- {}'.format(f))"""
+    return all_intervals
+
+if __name__ == '__main__':
+    files = 1000
+    generated = compute_intervals('output_folkrnnv2.txt', 1000)
+    print(generated)
+    original = compute_intervals('allabcwrepeats_parsed', 1000)
+    print(original)
