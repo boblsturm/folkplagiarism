f21db4dc2eb1d52031f40293762441ee47e28858
Nestor Napoles Lopez <napulen@gmail.com>
2020-02-04 21:15:09 -0500

    Adding script for textbased comparison of the training set

---
+++ similarity_of_training/textbased_comparison.py
@@ -0,0 +1,27 @@
+import music21
+import pprint
+import textdistance
+import itertools
+from multiprocessing import Pool
+
+FILENAME = 'allabcwrepeats_parsed'
+FILE_LIMIT = 10
+
+def compare_strings(idxa, a, idxb, b):
+    a = 'M:{}'.format(a.split('M:')[1])
+    b = 'M:{}'.format(b.split('M:')[1])
+    score = textdistance.levenshtein.normalized_similarity(a, b)
+    print(idxa, idxb, score)
+
+if __name__ == '__main__':
+    with open(FILENAME, encoding='utf-8') as f:
+        data = f.read()
+    # Files are delimited by a blank line (2 '\n's in a row )
+    files = data.split('\n\n')
+    # Files after being parsed by music21
+    if not FILE_LIMIT:
+        FILE_LIMIT = len(files)
+    combinations = itertools.combinations(range(FILE_LIMIT), 2)
+    comparisons = [(idxa, files[idxa], idxb, files[idxb]) for idxa, idxb in combinations]
+    p = Pool(4)
+    p.starmap(compare_strings, comparisons)
