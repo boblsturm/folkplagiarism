96f93878fc2daaa342fb8d93ae10474ed1f7f16f
Nestor Napoles Lopez <napulen@gmail.com>
2020-02-08 12:50:24 -0500

    Breaking the computation into subsets in case we run out of time in hpc

--- similarity_of_training/textbased_comparison.py
+++ similarity_of_training/textbased_comparison.py
@@ -4,14 +4,14 @@ import textdistance
 import itertools
 from multiprocessing import Pool
 
-FILENAME = 'allabcwrepeats_parsed'
+FILENAME = '../allabcwrepeats_parsed'
 FILE_LIMIT = 10000
 
 def compare_strings(idxa, a, idxb, b):
     a = 'M:{}'.format(a.split('M:')[1])
     b = 'M:{}'.format(b.split('M:')[1])
     score = textdistance.damerau_levenshtein.normalized_similarity(a, b)
-    print(idxa, idxb, score)
+    return (idxa, idxb, score)
 
 if __name__ == '__main__':
     with open(FILENAME, encoding='utf-8') as f:
@@ -23,5 +23,9 @@ if __name__ == '__main__':
         FILE_LIMIT = len(files)
     combinations = itertools.combinations(range(FILE_LIMIT), 2)
     comparisons = [(idxa, files[idxa], idxb, files[idxb]) for idxa, idxb in combinations]
+    subset_size = 10000
+    subsets = [comparisons[x:x+subset_size] for x in range(0, len(comparisons), subset_size)]
     p = Pool(32)
-    p.starmap(compare_strings, comparisons)
+    for idx, subset in enumerate(subsets):
+        print('subset_' + str(idx) + ' = ', p.starmap(compare_strings, subset))
+        print()
